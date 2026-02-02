009d7f4a1c9155380cef7a176e811417633541e2
Ruhulalemeen Mulla <ruhanmulla07@gmail.com>
2026-02-02 19:56:28 +0530

    Implement minimum cost subarray division algorithm

---
+++ 3000-3099/3013. Divide an Array Into Subarrays With Minimum Cost II.ts
@@ -0,0 +1,573 @@
+function minimumCost(nums: number[], k: number, dist: number): number {
+    --k;
+    const l = new TreapMultiSet<number>((a, b) => a - b);
+    const r = new TreapMultiSet<number>((a, b) => a - b);
+    let s = nums[0];
+    for (let i = 1; i < dist + 2; ++i) {
+        s += nums[i];
+        l.add(nums[i]);
+    }
+    const l2r = () => {
+        const x = l.pop()!;
+        s -= x;
+        r.add(x);
+    };
+    const r2l = () => {
+        const x = r.shift()!;
+        l.add(x);
+        s += x;
+    };
+    while (l.size > k) {
+        l2r();
+    }
+    let ans = s;
+    for (let i = dist + 2; i < nums.length; ++i) {
+        const x = nums[i - dist - 1];
+        if (l.has(x)) {
+            l.delete(x);
+            s -= x;
+        } else {
+            r.delete(x);
+        }
+        const y = nums[i];
+        if (y < l.last()!) {
+            l.add(y);
+            s += y;
+        } else {
+            r.add(y);
+        }
+        while (l.size < k) {
+            r2l();
+        }
+        while (l.size > k) {
+            l2r();
+        }
+        ans = Math.min(ans, s);
+    }
+    return ans;
+}
+
+type CompareFunction<T, R extends 'number' | 'boolean'> = (
+    a: T,
+    b: T,
+) => R extends 'number' ? number : boolean;
+
+interface ITreapMultiSet<T> extends Iterable<T> {
+    add: (...value: T[]) => this;
+    has: (value: T) => boolean;
+    delete: (value: T) => void;
+
+    bisectLeft: (value: T) => number;
+    bisectRight: (value: T) => number;
+
+    indexOf: (value: T) => number;
+    lastIndexOf: (value: T) => number;
+
+    at: (index: number) => T | undefined;
+    first: () => T | undefined;
+    last: () => T | undefined;
+
+    lower: (value: T) => T | undefined;
+    higher: (value: T) => T | undefined;
+    floor: (value: T) => T | undefined;
+    ceil: (value: T) => T | undefined;
+
+    shift: () => T | undefined;
+    pop: (index?: number) => T | undefined;
+
+    count: (value: T) => number;
+
+    keys: () => IterableIterator<T>;
+    values: () => IterableIterator<T>;
+    rvalues: () => IterableIterator<T>;
+    entries: () => IterableIterator<[number, T]>;
+
+    readonly size: number;
+}
+
+class TreapNode<T = number> {
+    value: T;
+    count: number;
+    size: number;
+    priority: number;
+    left: TreapNode<T> | null;
+    right: TreapNode<T> | null;
+
+    constructor(value: T) {
+        this.value = value;
+        this.count = 1;
+        this.size = 1;
+        this.priority = Math.random();
+        this.left = null;
+        this.right = null;
+    }
+
+    static getSize(node: TreapNode<any> | null): number {
+        return node?.size ?? 0;
+    }
+
+    static getFac(node: TreapNode<any> | null): number {
+        return node?.priority ?? 0;
+    }
+
+    pushUp(): void {
+        let tmp = this.count;
+        tmp += TreapNode.getSize(this.left);
+        tmp += TreapNode.getSize(this.right);
+        this.size = tmp;
+    }
+
+    rotateRight(): TreapNode<T> {
+        let node: TreapNode<T> = this;
+        const left = node.left;
+        node.left = left?.right ?? null;
+        left && (left.right = node);
+        left && (node = left);
+        node.right?.pushUp();
+        node.pushUp();
+        return node;
+    }
+
+    rotateLeft(): TreapNode<T> {
+        let node: TreapNode<T> = this;
+        const right = node.right;
+        node.right = right?.left ?? null;
+        right && (right.left = node);
+        right && (node = right);
+        node.left?.pushUp();
+        node.pushUp();
+        return node;
+    }
+}
+
+class TreapMultiSet<T = number> implements ITreapMultiSet<T> {
+    private readonly root: TreapNode<T>;
+    private readonly compareFn: CompareFunction<T, 'number'>;
+    private readonly leftBound: T;
+    private readonly rightBound: T;
+
+    constructor(compareFn?: CompareFunction<T, 'number'>);
+    constructor(compareFn: CompareFunction<T, 'number'>, leftBound: T, rightBound: T);
+    constructor(
+        compareFn: CompareFunction<T, any> = (a: any, b: any) => a - b,
+        leftBound: any = -Infinity,
+        rightBound: any = Infinity,
+    ) {
+        this.root = new TreapNode<T>(rightBound);
+        this.root.priority = Infinity;
+        this.root.left = new TreapNode<T>(leftBound);
+        this.root.left.priority = -Infinity;
+        this.root.pushUp();
+
+        this.leftBound = leftBound;
+        this.rightBound = rightBound;
+        this.compareFn = compareFn;
+    }
+
+    get size(): number {
+        return this.root.size - 2;
+    }
+
+    get height(): number {
+        const getHeight = (node: TreapNode<T> | null): number => {
+            if (node == null) return 0;
+            return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        };
+
+        return getHeight(this.root);
+    }
+
+    
+    has(value: T): boolean {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): boolean => {
+            if (node == null) return false;
+            if (compare(node.value, value) === 0) return true;
+            if (compare(node.value, value) < 0) return dfs(node.right, value);
+            return dfs(node.left, value);
+        };
+
+        return dfs(this.root, value);
+    }
+
+    
+    add(...values: T[]): this {
+        const compare = this.compareFn;
+        const dfs = (
+            node: TreapNode<T> | null,
+            value: T,
+            parent: TreapNode<T>,
+            direction: 'left' | 'right',
+        ): void => {
+            if (node == null) return;
+            if (compare(node.value, value) === 0) {
+                node.count++;
+                node.pushUp();
+            } else if (compare(node.value, value) > 0) {
+                if (node.left) {
+                    dfs(node.left, value, node, 'left');
+                } else {
+                    node.left = new TreapNode(value);
+                    node.pushUp();
+                }
+
+                if (TreapNode.getFac(node.left) > node.priority) {
+                    parent[direction] = node.rotateRight();
+                }
+            } else if (compare(node.value, value) < 0) {
+                if (node.right) {
+                    dfs(node.right, value, node, 'right');
+                } else {
+                    node.right = new TreapNode(value);
+                    node.pushUp();
+                }
+
+                if (TreapNode.getFac(node.right) > node.priority) {
+                    parent[direction] = node.rotateLeft();
+                }
+            }
+            parent.pushUp();
+        };
+
+        values.forEach(value => dfs(this.root.left, value, this.root, 'left'));
+        return this;
+    }
+
+    
+    delete(value: T): void {
+        const compare = this.compareFn;
+        const dfs = (
+            node: TreapNode<T> | null,
+            value: T,
+            parent: TreapNode<T>,
+            direction: 'left' | 'right',
+        ): void => {
+            if (node == null) return;
+
+            if (compare(node.value, value) === 0) {
+                if (node.count > 1) {
+                    node.count--;
+                    node?.pushUp();
+                } else if (node.left == null && node.right == null) {
+                    parent[direction] = null;
+                } else {
+                    if (
+                        node.right == null ||
+                        TreapNode.getFac(node.left) > TreapNode.getFac(node.right)
+                    ) {
+                        parent[direction] = node.rotateRight();
+                        dfs(parent[direction]?.right ?? null, value, parent[direction]!, 'right');
+                    } else {
+                        parent[direction] = node.rotateLeft();
+                        dfs(parent[direction]?.left ?? null, value, parent[direction]!, 'left');
+                    }
+                }
+            } else if (compare(node.value, value) > 0) {
+                dfs(node.left, value, node, 'left');
+            } else if (compare(node.value, value) < 0) {
+                dfs(node.right, value, node, 'right');
+            }
+
+            parent?.pushUp();
+        };
+
+        dfs(this.root.left, value, this.root, 'left');
+    }
+
+    
+    bisectLeft(value: T): number {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): number => {
+            if (node == null) return 0;
+
+            if (compare(node.value, value) === 0) {
+                return TreapNode.getSize(node.left);
+            } else if (compare(node.value, value) > 0) {
+                return dfs(node.left, value);
+            } else if (compare(node.value, value) < 0) {
+                return dfs(node.right, value) + TreapNode.getSize(node.left) + node.count;
+            }
+
+            return 0;
+        };
+
+        return dfs(this.root, value) - 1;
+    }
+
+    
+    bisectRight(value: T): number {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): number => {
+            if (node == null) return 0;
+
+            if (compare(node.value, value) === 0) {
+                return TreapNode.getSize(node.left) + node.count;
+            } else if (compare(node.value, value) > 0) {
+                return dfs(node.left, value);
+            } else if (compare(node.value, value) < 0) {
+                return dfs(node.right, value) + TreapNode.getSize(node.left) + node.count;
+            }
+
+            return 0;
+        };
+        return dfs(this.root, value) - 1;
+    }
+
+    
+    indexOf(value: T): number {
+        const compare = this.compareFn;
+        let isExist = false;
+
+        const dfs = (node: TreapNode<T> | null, value: T): number => {
+            if (node == null) return 0;
+
+            if (compare(node.value, value) === 0) {
+                isExist = true;
+                return TreapNode.getSize(node.left);
+            } else if (compare(node.value, value) > 0) {
+                return dfs(node.left, value);
+            } else if (compare(node.value, value) < 0) {
+                return dfs(node.right, value) + TreapNode.getSize(node.left) + node.count;
+            }
+
+            return 0;
+        };
+        const res = dfs(this.root, value) - 1;
+        return isExist ? res : -1;
+    }
+
+    
+    lastIndexOf(value: T): number {
+        const compare = this.compareFn;
+        let isExist = false;
+
+        const dfs = (node: TreapNode<T> | null, value: T): number => {
+            if (node == null) return 0;
+
+            if (compare(node.value, value) === 0) {
+                isExist = true;
+                return TreapNode.getSize(node.left) + node.count - 1;
+            } else if (compare(node.value, value) > 0) {
+                return dfs(node.left, value);
+            } else if (compare(node.value, value) < 0) {
+                return dfs(node.right, value) + TreapNode.getSize(node.left) + node.count;
+            }
+
+            return 0;
+        };
+
+        const res = dfs(this.root, value) - 1;
+        return isExist ? res : -1;
+    }
+
+    
+    at(index: number): T | undefined {
+        if (index < 0) index += this.size;
+        if (index < 0 || index >= this.size) return undefined;
+
+        const dfs = (node: TreapNode<T> | null, rank: number): T | undefined => {
+            if (node == null) return undefined;
+
+            if (TreapNode.getSize(node.left) >= rank) {
+                return dfs(node.left, rank);
+            } else if (TreapNode.getSize(node.left) + node.count >= rank) {
+                return node.value;
+            } else {
+                return dfs(node.right, rank - TreapNode.getSize(node.left) - node.count);
+            }
+        };
+
+        const res = dfs(this.root, index + 2);
+        return ([this.leftBound, this.rightBound] as any[]).includes(res) ? undefined : res;
+    }
+
+    
+    lower(value: T): T | undefined {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): T | undefined => {
+            if (node == null) return undefined;
+            if (compare(node.value, value) >= 0) return dfs(node.left, value);
+
+            const tmp = dfs(node.right, value);
+            if (tmp == null || compare(node.value, tmp) > 0) {
+                return node.value;
+            } else {
+                return tmp;
+            }
+        };
+
+        const res = dfs(this.root, value) as any;
+        return res === this.leftBound ? undefined : res;
+    }
+
+    
+    higher(value: T): T | undefined {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): T | undefined => {
+            if (node == null) return undefined;
+            if (compare(node.value, value) <= 0) return dfs(node.right, value);
+
+            const tmp = dfs(node.left, value);
+
+            if (tmp == null || compare(node.value, tmp) < 0) {
+                return node.value;
+            } else {
+                return tmp;
+            }
+        };
+
+        const res = dfs(this.root, value) as any;
+        return res === this.rightBound ? undefined : res;
+    }
+
+   
+    floor(value: T): T | undefined {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): T | undefined => {
+            if (node == null) return undefined;
+            if (compare(node.value, value) === 0) return node.value;
+            if (compare(node.value, value) >= 0) return dfs(node.left, value);
+
+            const tmp = dfs(node.right, value);
+            if (tmp == null || compare(node.value, tmp) > 0) {
+                return node.value;
+            } else {
+                return tmp;
+            }
+        };
+
+        const res = dfs(this.root, value) as any;
+        return res === this.leftBound ? undefined : res;
+    }
+
+    
+    ceil(value: T): T | undefined {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): T | undefined => {
+            if (node == null) return undefined;
+            if (compare(node.value, value) === 0) return node.value;
+            if (compare(node.value, value) <= 0) return dfs(node.right, value);
+
+            const tmp = dfs(node.left, value);
+
+            if (tmp == null || compare(node.value, tmp) < 0) {
+                return node.value;
+            } else {
+                return tmp;
+            }
+        };
+
+        const res = dfs(this.root, value) as any;
+        return res === this.rightBound ? undefined : res;
+    }
+
+    
+    first(): T | undefined {
+        const iter = this.inOrder();
+        iter.next();
+        const res = iter.next().value;
+        return res === this.rightBound ? undefined : res;
+    }
+
+    
+    last(): T | undefined {
+        const iter = this.reverseInOrder();
+        iter.next();
+        const res = iter.next().value;
+        return res === this.leftBound ? undefined : res;
+    }
+
+    
+    shift(): T | undefined {
+        const first = this.first();
+        if (first === undefined) return undefined;
+        this.delete(first);
+        return first;
+    }
+
+    
+    pop(index?: number): T | undefined {
+        if (index == null) {
+            const last = this.last();
+            if (last === undefined) return undefined;
+            this.delete(last);
+            return last;
+        }
+
+        const toDelete = this.at(index);
+        if (toDelete == null) return;
+        this.delete(toDelete);
+        return toDelete;
+    }
+
+    
+    count(value: T): number {
+        const compare = this.compareFn;
+        const dfs = (node: TreapNode<T> | null, value: T): number => {
+            if (node == null) return 0;
+            if (compare(node.value, value) === 0) return node.count;
+            if (compare(node.value, value) < 0) return dfs(node.right, value);
+            return dfs(node.left, value);
+        };
+
+        return dfs(this.root, value);
+    }
+
+    *[Symbol.iterator](): Generator<T, any, any> {
+        yield* this.values();
+    }
+
+    
+    *keys(): Generator<T, any, any> {
+        yield* this.values();
+    }
+
+    
+    *values(): Generator<T, any, any> {
+        const iter = this.inOrder();
+        iter.next();
+        const steps = this.size;
+        for (let _ = 0; _ < steps; _++) {
+            yield iter.next().value;
+        }
+    }
+
+    
+    *rvalues(): Generator<T, any, any> {
+        const iter = this.reverseInOrder();
+        iter.next();
+        const steps = this.size;
+        for (let _ = 0; _ < steps; _++) {
+            yield iter.next().value;
+        }
+    }
+
+    *entries(): IterableIterator<[number, T]> {
+        const iter = this.inOrder();
+        iter.next();
+        const steps = this.size;
+        for (let i = 0; i < steps; i++) {
+            yield [i, iter.next().value];
+        }
+    }
+
+    private *inOrder(root: TreapNode<T> | null = this.root): Generator<T, any, any> {
+        if (root == null) return;
+        yield* this.inOrder(root.left);
+        const count = root.count;
+        for (let _ = 0; _ < count; _++) {
+            yield root.value;
+        }
+        yield* this.inOrder(root.right);
+    }
+
+    private *reverseInOrder(root: TreapNode<T> | null = this.root): Generator<T, any, any> {
+        if (root == null) return;
+        yield* this.reverseInOrder(root.right);
+        const count = root.count;
+        for (let _ = 0; _ < count; _++) {
+            yield root.value;
+        }
+        yield* this.reverseInOrder(root.left);
+    }
+}
